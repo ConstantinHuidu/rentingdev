f8cbed0767f9102c02108f934355a49934d6fae8
ConstantinHuidu <constantin.huidu@gmail.com>
2023-03-21 09:26:23 +0200

    mongoURL to env.local

--- helpers/db.tsx
+++ helpers/db.tsx
@@ -1,9 +1,8 @@
 import { MongoClient } from "mongodb";
 
 export async function connectToDB() {
-  const client = await MongoClient.connect(
-    "mongodb+srv://rentingDev:1ZwznCHF5wATnrkf@cluster0.p01xqcc.mongodb.net/rentingdev?retryWrites=true&w=majority"
-  );
+  const MONGODB_URL = process.env.MONGODB_URI as string;
+  const client = await MongoClient.connect(MONGODB_URL);
 
   return client;
 }
