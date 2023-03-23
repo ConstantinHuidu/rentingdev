f87c287c2bedd2b02684bda92f1160b8cce7aeb9
alexcancel1995 <alexandru.cancel@yahoo.com>
2023-03-23 17:38:45 +0200

    update also on mobile

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -70,56 +70,64 @@ const Header = ({ darkMode, toggleDarkMode }: HeaderPropsType) => {
           {/* <button onClick={toggleDarkMode}>Toggle DarkMode</button> */}
           <DarkModeButton toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
         </ul>
-        <button className="block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:bg-gray-200 group">
-          <div className="w-7 h-1 bg-gray-600 mb-1"></div>
-          <div className="w-7 h-1 bg-gray-600 mb-1"></div>
-          <div className="w-7 h-1 bg-gray-600"></div>
-          <div className="absolute top-14 -right-full h-screen w-full bg-white border opacity-0 group-focus:right-0 group-focus:opacity-100 group-focus:z-10 transition-all duration-300">
-            <ul className="flex flex-col items-center w-full text-base pt-10 uppercase">
-              <Link
-                className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
-                href="/about"
-              >
-                About
-              </Link>
-              <Link
-                className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
-                href="/products"
-              >
-                Products
-              </Link>
-              <Link
-                className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
-                href="/contact"
-              >
-                Contact
-              </Link>
-              {!session?.user && (
+        <div className="flex md:hidden justify-center items-center dark:text-white">
+          <div className="block md:hidden">
+            <DarkModeButton
+              toggleDarkMode={toggleDarkMode}
+              darkMode={darkMode}
+            />
+          </div>
+          <button className="block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:bg-gray-200 group">
+            <div className="w-7 h-1 bg-gray-600 mb-1 dark:bg-white"></div>
+            <div className="w-7 h-1 bg-gray-600 mb-1 dark:bg-white"></div>
+            <div className="w-7 h-1 bg-gray-600 dark:bg-white"></div>
+            <div className="absolute top-14 -right-full h-screen w-full bg-white border opacity-0 group-focus:right-0 group-focus:opacity-100 group-focus:z-10 transition-all duration-300">
+              <ul className="flex flex-col items-center w-full text-base pt-10 uppercase">
                 <Link
                   className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
-                  href="/login"
+                  href="/about"
                 >
-                  Log in
+                  About
                 </Link>
-              )}
-              {/* <Link
+                <Link
+                  className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
+                  href="/products"
+                >
+                  Products
+                </Link>
+                <Link
+                  className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
+                  href="/contact"
+                >
+                  Contact
+                </Link>
+                {!session?.user && (
+                  <Link
+                    className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
+                    href="/login"
+                  >
+                    Log in
+                  </Link>
+                )}
+                {/* <Link
                 className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
                 href="/signup"
               >
                 Signup
               </Link> */}
-              {session?.user && (
-                <Link
-                  onClick={handleSignOut}
-                  className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
-                  href="/signup"
-                >
-                  Sign out
-                </Link>
-              )}
-            </ul>
-          </div>
-        </button>
+                {session?.user && (
+                  <Link
+                    onClick={handleSignOut}
+                    className="hover:bg-gray-200 rounded-lg py-4 px-6 w-full cursor-pointer"
+                    href="/signup"
+                  >
+                    Sign out
+                  </Link>
+                )}
+              </ul>
+            </div>
+          </button>
+        </div>
       </div>
     </nav>
   );
