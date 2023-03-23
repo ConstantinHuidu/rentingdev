14edd7f5a5b256c2abd42f49a03d0f298719ae0d
ConstantinHuidu <constantin.huidu@gmail.com>
2023-03-23 16:18:11 +0200

    login and signup protected routes

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,6 @@
 import Signup from "@/components/signup/Signup";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 import React from "react";
 
@@ -14,6 +16,15 @@ const signup = () => {
       <Signup />
     </>
   );
+};
+
+// === SERVER-SIDE REDIRECT IF USER IS AUTHENTICATED ===
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+  if (session) {
+    return { redirect: { destination: "/", permanent: false } };
+  }
+  return { props: { session } };
 };
 
 export default signup;
