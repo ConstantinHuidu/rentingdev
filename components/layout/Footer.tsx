825026446b633f701c2a64e6bf7e9c99acfdf795
victoriazag <43202910+victoriazag@users.noreply.github.com>
2023-03-24 14:57:03 +0200

    footer updates (#10)

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,9 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <div className="w-full h-[10vh] bg-white dark:bg-gray-900 dark:text-white">
-      Footer
+      <div className="flex justify-center items-center ">
+        <span className="bg-yellow-600 h-0.5 w-4/5 mb-3"></span>
+      </div>
+      <div className="container px-6 flex justify-between items-center m-auto w-full uppercase lg:font-semibold flex-col lg:flex-row text-gray-700 dark:text-white text-xs">
+        <div>
+          <Link
+            className="hover:bg-gray-200  dark:hover:text-black rounded-lg py-2 px-6 w-full cursor-pointer "
+            href="/about"
+          >
+            Help
+          </Link>
+          <Link
+            className="hover:bg-gray-200  dark:hover:text-black rounded-lg py-2 px-6 w-full cursor-pointer"
+            href="/about"
+          >
+            Privacy policy
+          </Link>
+        </div>
+        <p>Copyright@2023</p>
+      </div>
     </div>
   );
 };
