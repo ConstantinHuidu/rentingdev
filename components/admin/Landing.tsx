837f328a41a14959137f2516e0d9de359f53ea62
victoriazag <vica.zagornean@gmail.com>
2023-05-01 22:12:38 +0300

    modal added 1.0

--- components/admin/Landing.tsx
+++ components/admin/Landing.tsx
@@ -1,39 +1,49 @@
+import { useState } from "react";
 import { DUMMY_PRODUCTS_INFO } from "./DummyProductDetails";
 import Image from "next/image";
+import Modal from "./components/Modal";
+import Search from "./components/Search";
+import Pagination from "./components/Pagination";
 
 const Landing = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <h1 className="text-xl mb-2 mx-auto text-center">Products list</h1>
-
-      {/* search  */}
-
-      <div className="relative flex items-center mt-4 md:mt-0 w-10/12 m-auto my-4">
-        <span className="absolute">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
-        </span>
-
-        <input
-          type="text"
-          placeholder="Search"
-          className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-yellow-600 dark:focus:border-yellow-600 focus:ring-yellow-600 focus:outline-none focus:ring focus:ring-opacity-40"
-        />
+      <div className="flex justify-between items-center mx-auto my-4 w-10/12">
+        <Search />
+        <button
+          className="btn bg-white text-gray-500"
+          onClick={() => setOpen(true)}
+        >
+          +
+        </button>
       </div>
 
-      {/* end search  */}
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div className="mx-auro w-56">
+          <div className="mx-auto my-4 w-48">
+            <h3 className="text-lg font-black text-gray-800">
+              Add new product
+            </h3>
+            <p className="text-sm text-gray-500">Please add a new product</p>
+          </div>
+          <div className="flex gap-4">
+            <button
+              className="btn w-full text-white bg-red-600 shadow-red-400/40"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </button>
+            <button
+              className="btn w-full  bg-white text-gray-500"
+              onClick={() => setOpen(false)}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </Modal>
 
       <div className="overflow-auto rounded-lg shadow hidden md:block w-10/12 m-auto">
         <table className="table-auto w-full">
@@ -64,7 +74,10 @@ const Landing = () => {
           </thead>
           <tbody className="divide-y divide-gray-100 text-gray-700 text-sm font-light">
             {DUMMY_PRODUCTS_INFO.map((item) => (
-              <tr className="border-b border-gray-200 hover:bg-gray-100">
+              <tr
+                className="border-b border-gray-200 hover:bg-gray-100"
+                key={item.id}
+              >
                 <td className="p-3 text-gray-700 whitespace-nowrap">
                   <a
                     href="#"
@@ -100,15 +113,15 @@ const Landing = () => {
                         stroke="currentColor"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                         />
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
                         />
                       </svg>
@@ -121,9 +134,9 @@ const Landing = () => {
                         stroke="currentColor"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
                         />
                       </svg>
@@ -136,9 +149,9 @@ const Landing = () => {
                         stroke="currentColor"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                         />
                       </svg>
@@ -178,15 +191,15 @@ const Landing = () => {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                   />
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
                   />
                 </svg>
@@ -199,9 +212,9 @@ const Landing = () => {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
                   />
                 </svg>
@@ -214,9 +227,9 @@ const Landing = () => {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                   />
                 </svg>
@@ -225,61 +238,7 @@ const Landing = () => {
           </div>
         ))}
       </div>
-      {/* pagination */}
-      <div className="w-10/12 m-auto my-8 sm:flex sm:items-center sm:justify-between ">
-        <div className="text-sm text-gray-500 dark:text-gray-400">
-          Page{" "}
-          <span className="font-medium text-gray-700 dark:text-gray-100">
-            1 of 10
-          </span>
-        </div>
-
-        <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
-          <a
-            href="#"
-            className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-5 h-5 rtl:-scale-x-100"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-              />
-            </svg>
-
-            <span>previous</span>
-          </a>
-
-          <a
-            href="#"
-            className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-          >
-            <span>Next</span>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-5 h-5 rtl:-scale-x-100"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-              />
-            </svg>
-          </a>
-        </div>
-      </div>
+      <Pagination />
     </div>
   );
 };
